efc14e04651c7e6bb9a055e7c4b3dc1db7696615
luksteroz <lukasz.jeleniewicz@gmail.com>
2017-05-28 19:19:19 +0200

    change color icon

--- js/app.js
+++ js/app.js
@@ -1,61 +1,67 @@
-document.addEventListener("DOMContentLoaded", function () {
+(function() {
+    'use strict';
 
-    const buttonMenu = document.querySelector(".dropdown-toggle");
-    const dropMenu = document.querySelector(".hamburger-menu");
-    const costHistory = document.querySelectorAll(".cost");
-    const buttonSearch = document.querySelector(".glyphicon-search");
-    const inputSearch = document.getElementById("search");
-    const historyDetail = document.querySelectorAll(".glyphicon-menu-down");
-    const dropHistory = document.querySelectorAll(".details-history");
+    function onClickMenuHandler(event) {
+        event.preventDefault();
 
-    for (var i = 0; i < costHistory.length; i++) {
-        if (parseInt(costHistory[i].dataset.value) > 0) {
-            costHistory[i].style.color = "green";
-        } else{
-            costHistory[i].style.color = "black";
-        }
+        var parent = this.parentElement;
+
+        dropMenu.classList.toggle('open');
+        parent.classList.toggle('open');
     }
 
-    console.log(dropMenu);
+    function domContentLoadedHandler(event) {
+        var buttonMenu = document.querySelector(".dropdown-toggle"),
+            dropMenu = document.querySelector(".hamburger-menu"),
+            costHistory = document.querySelectorAll(".cost"),
+            buttonSearch = document.querySelector(".glyphicon-search"),
+            inputSearch = document.getElementById("search"),
+            historyDetail = document.querySelectorAll(".glyphicon-menu-down"),
+            dropHistory = document.querySelectorAll(".details-history");
 
-    buttonMenu.addEventListener("click", function () {
-        event.preventDefault();
-        dropMenu.classList.toggle("open");
-        this.parentElement.classList.toggle("open");
-    });
+        for (var i = 0; i < costHistory.length; i++) {
+            if (parseInt(costHistory[i].dataset.value) > 0) {
+                costHistory[i].style.color = "green";
+            } else{
+                costHistory[i].style.color = "black";
+            }
+        }
 
+        console.log(dropMenu);
 
+        buttonMenu.addEventListener('click', onClickMenuHandler);
 
-    buttonSearch.addEventListener("click", function () {
-        event.preventDefault();
-        inputSearch.classList.toggle("visible");
-    });
-
-    const power = document.querySelector(".glyphicon-off")
-    power.addEventListener("click", function () {
-        event.preventDefault();
-        this.style.color = this.style.color === "red" ? "white" : "red";
-    });
-
-
-    for (var i = 0; i < dropHistory.length; i++) {
-
-        historyDetail[i].addEventListener("click", function(){
+        buttonSearch.addEventListener("click", function () {
             event.preventDefault();
-            this.parentElement.nextElementSibling.classList.toggle("show");
-        })
+            inputSearch.classList.toggle("visible");
+        });
+
+        var power = document.querySelector(".glyphicon-off")
+        power.addEventListener("click", function () {
+            event.preventDefault();
+            this.style.color = this.style.color === "red" ? "white" : "red";
+        });
+
+
+        for (var i = 0; i < dropHistory.length; i++) {
+
+            historyDetail[i].addEventListener("click", function(){
+                event.preventDefault();
+                this.parentElement.nextElementSibling.classList.toggle("show");
+            })
+        }
+
+        var toggleButton = document.querySelector(".slider")
+        var showChart = document.getElementById("chart");
+        toggleButton.addEventListener("click", function(){
+            if (showChart.style.display == "block") {
+                showChart.style.display = "none";
+            }else {
+                showChart.style.display = "block";
+
+            }
+        });
     }
 
-    const toggleButton = document.querySelector(".slider")
-    const showChart = document.getElementById("chart");
-    toggleButton.addEventListener("click", function(){
-        if (showChart.style.display == "block") {
-            showChart.style.display = "none";
-        }else {
-            showChart.style.display = "block";
-
-        }
-    });
-
-
-});
+    document.addEventListener('DOMContentLoaded', domContentLoadedHandler);
+})();
