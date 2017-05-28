0b2b163d1260b9e28369c0f56185f9c1e21b1b9e
luksteroz <lukasz.jeleniewicz@gmail.com>
2017-05-29 00:44:45 +0200

    change badge colors

--- js/app.js
+++ js/app.js
@@ -1,14 +1,6 @@
 (function() {
     'use strict';
 
-    function onClickMenuHandler(event) {
-        event.preventDefault();
-
-        var parent = this.parentElement;
-
-        dropMenu.classList.toggle('open');
-        parent.classList.toggle('open');
-    }
 
     function domContentLoadedHandler(event) {
         var buttonMenu = document.querySelector(".dropdown-toggle"),
@@ -17,50 +9,89 @@
             buttonSearch = document.querySelector(".glyphicon-search"),
             inputSearch = document.getElementById("search"),
             historyDetail = document.querySelectorAll(".glyphicon-menu-down"),
-            dropHistory = document.querySelectorAll(".details-history");
+            dropHistory = document.querySelectorAll(".details-history"),
+            toggleButton = document.querySelector(".slider"),
+            showChart = document.getElementById("chart"),
+            toggleButton = document.querySelector(".slider"),
+            showChart = document.getElementById("chart"),
+            power = document.querySelector(".off"),
+            menuItem = document.querySelectorAll(".menu-item");
 
         for (var i = 0; i < costHistory.length; i++) {
+
             if (parseInt(costHistory[i].dataset.value) > 0) {
                 costHistory[i].style.color = "green";
             } else{
                 costHistory[i].style.color = "black";
             }
-        }
 
-        console.log(dropMenu);
+        }
 
         buttonMenu.addEventListener('click', onClickMenuHandler);
 
-        buttonSearch.addEventListener("click", function () {
+        buttonSearch.addEventListener("click", onClickSearchHandler);
+
+        power.addEventListener("click", onClickPowerHandler);
+
+        toggleButton.addEventListener("click", onClickToggleChartHandler);
+
+
+
+        function onClickMenuHandler(event) {
             event.preventDefault();
-            inputSearch.classList.toggle("visible");
-        });
 
-        var power = document.querySelector(".glyphicon-off")
-        power.addEventListener("click", function () {
-            event.preventDefault();
-            this.style.color = this.style.color === "red" ? "white" : "red";
-        });
+            var parent = this.parentElement;
 
-
-        for (var i = 0; i < dropHistory.length; i++) {
-
-            historyDetail[i].addEventListener("click", function(){
-                event.preventDefault();
-                this.parentElement.nextElementSibling.classList.toggle("show");
-            })
+            dropMenu.classList.toggle('open');
+            parent.classList.toggle('open');
         }
 
-        var toggleButton = document.querySelector(".slider")
-        var showChart = document.getElementById("chart");
-        toggleButton.addEventListener("click", function(){
+        function onClickSearchHandler(event){
+            event.preventDefault();
+
+            inputSearch.classList.toggle("visible");
+
+        }
+
+        function onClickPowerHandler(event) {
+
+            event.preventDefault();
+            this.style.color = this.style.color === "red" ? "white" : "red";
+
+        }
+
+        function onClickToggleChartHandler(event){
             if (showChart.style.display == "block") {
                 showChart.style.display = "none";
             }else {
                 showChart.style.display = "block";
-
             }
-        });
+        }
+
+        for (var i = 0; i < dropHistory.length; i++) {
+
+            historyDetail[i].addEventListener("click", function(event){
+
+                event.preventDefault();
+                var elementSibling = this.parentElement.nextElementSibling;
+                elementSibling.classList.toggle("show");
+
+            })
+        }
+
+
+        for (var j = 0; j < menuItem.length; j++) {
+
+            menuItem[j].addEventListener("click", function(event){
+
+                for (var k = 0; k < menuItem.length; k++) {
+
+                    menuItem[k].classList.remove("active-menu");
+                }
+
+                this.classList.add("active-menu")
+            });
+        }
     }
 
     document.addEventListener('DOMContentLoaded', domContentLoadedHandler);
